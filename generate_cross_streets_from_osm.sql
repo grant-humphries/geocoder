303787dc1e245f2fac396ee7ea2dd1a5a09b8a20
grant-humphries <grant.humphries@gmail.com>
2013-12-18 11:25:26 -0800

    rewrote generate cross streets script with create table as and improved performance

--- generate_cross_streets_from_osm.sql
+++ generate_cross_streets_from_osm.sql
@@ -1,22 +1,12 @@
-create table as osm.cross_streets(
-	X numeric,
-	Y numeric,
-	osm_node_id bigint,
-	street_1 text,
-	street_2 text
-);
-
-CREATE TEMP TABLE intersections(node_id bigint);
-insert into intersections 
+create temp table intersections as
 	select node_id from osm.way_nodes wn
 	where exists (select null from osm.way_tags wt
 					where k = 'name'
 					and wn.way_id = wt.way_id)
 	group by node_id having count(way_id) > 1;
 
-CREATE TEMP TABLE named_ints(geom geometry, node_id bigint, name text);
-insert into named_ints
-	select n.geom, n.id, wt.v
+create temp table named_ints as
+	select n.geom, n.id as node_id, wt.v as name
 		from osm.nodes n, osm.way_nodes wn, osm.way_tags wt
 		where n.id in (select node_id from intersections)
 			and n.id = wn.node_id
@@ -25,8 +15,9 @@ insert into named_ints
 		group by n.geom, n.id, wt.v
 		order by n.id, wt.v;
 
-insert into osm.cross_streets
-	select ST_X(ni1.geom), ST_Y(ni1.geom), ni1.node_id, ni1.name, ni2.name
+drop table if exists osm.cross_streets cascade;
+create table osm.cross_streets with oids as
+	select ST_X(ni1.geom) as x, ST_Y(ni1.geom) as y, ni1.node_id, ni1.name as street_1, ni2.name as street_2
 		from named_ints ni1, named_ints ni2
 		where ni1.node_id = ni2.node_id
 			and ni1.name != ni2.name
@@ -34,3 +25,5 @@ insert into osm.cross_streets
 
 drop table intersections;
 drop table named_ints;
+
+-- ran in 7401 ms on 12/18/2013
