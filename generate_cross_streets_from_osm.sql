a773a792ae838c47964fb418390a246b6053a785
unknown <humphrig@PC25029.tri-met.org>
2014-03-06 01:22:01 +0000

    renamed temp table

--- generate_cross_streets_from_osm.sql
+++ generate_cross_streets_from_osm.sql
@@ -5,7 +5,7 @@ create temp table intersections as
 					and wn.way_id = wt.way_id)
 	group by node_id having count(way_id) > 1;
 
-create temp table named_ints as
+create temp table named_intersections as
 	select n.geom, n.id as node_id, wt.v as name
 		from osm.nodes n, osm.way_nodes wn, osm.way_tags wt
 		where n.id in (select node_id from intersections)
@@ -18,12 +18,12 @@ create temp table named_ints as
 drop table if exists osm.cross_streets cascade;
 create table osm.cross_streets with oids as
 	select ST_X(ni1.geom) as x, ST_Y(ni1.geom) as y, ni1.node_id, ni1.name as street_1, ni2.name as street_2
-		from named_ints ni1, named_ints ni2
+		from named_intersections ni1, named_intersections ni2
 		where ni1.node_id = ni2.node_id
 			and ni1.name != ni2.name
 		order by ni1.name, ni2.name;
 
 drop table intersections;
-drop table named_ints;
+drop table named_intersections;
 
 -- ran in 7401 ms on 12/18/2013
