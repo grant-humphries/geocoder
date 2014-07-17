b31f9ea39979cf929ee1cb6f5c24a295c02eeaff
grant-humphries <grant.humphries@gmail.com>
2014-07-16 18:04:26 -0700

    writing batch file to automate this thing

--- bin/generate_osm_cross_streets_shapefile.bat
+++ bin/generate_osm_cross_streets_shapefile.bat
@@ -1,8 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-::Set workspace
-set workspace=G:\PUBLIC\GIS_Projects\Geocoding\TriMet_Geocoder\git\geocoder
+::Set workspaces
+set workspace=G:\PUBLIC\GIS_Projects\Geocoding\TriMet_Geocoder
+set code_workspace=%workspace%\git\geocoder
+set data_workspace-%workspace%\data
 
 ::Set postgres parameters
 set pg_host=maps10.trimet.org
@@ -17,3 +19,16 @@ set /p pgpassword="Enter postgres password:"
 set x_streets_script=%workspace%\generate_cross_streets_from_osm.sql
 psql -h %pg_host% -d %db_name% -U %pg_user% -f %x_streets_script%
 
+::Launch python scripts that abbreviate the lengthy OSM versions of street names
+set renamer_script=%code_workspace%\abbreviate_street_names\rename_streets.py
+python %renamer_script%
+
+::Optionally run arcpy script that numerous jurisdictional datasets into two shapefiles
+set /p update_jurisdictional_data="Has any of the underlying jurisdictional data been updated?  Type 'y' (no quotes) to run script that will integrate that new data, type anything else to skip it"
+
+set jurisdiction_compilation_script=%code_workspace%/add_jursidictional_info/compile_jurisdictional_data.py
+if %update_jurisdictional_data%==y python %jurisdiction_compilation_script%
+
+::Load the jurisdictional shapefiles into postgis
+set srid=
+shp2pgsql -s %srid% -d -I %data_workspace%\ | psql -q -h %pg_host% -U %pg_user% -d %db_name%
